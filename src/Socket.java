0afada95bdd9c8e465532807780b5aadfda022dd
Thaifactory <michael.ratke@t-online.de>
2016-12-23 00:03:50 +0100

    Test

--- src/Socket.java
+++ src/Socket.java
@@ -1,4 +1,12 @@
+import java.io.BufferedReader;
+import java.io.PrintStream;
 
 public class Socket extends Thread {
+	Socket socket = null;
+	BufferedReader input = null;
+	PrintStream output = null;
 	
+	public Socket(Socket socket) {
+		
+	}
 }
