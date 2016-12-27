144cf5311aa5567fb20587f5e1ee8ae78ad1ec46
Thaifactory <michael.ratke@t-online.de>
2016-12-28 00:31:03 +0100

    Server

--- src/Server.java
+++ src/Server.java
@@ -6,39 +6,56 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-	public static void main(String[] args) {
-		int port = 0;
-		ServerSocket server = null;
-		if(args.length > 1) {
-			throw new IllegalArgumentException("Too many argumants!");
-		}
+	ServerSocket serverSocket;
+	boolean terminate = false;
+
+	public Server(int port) {
+		terminate = false;
+
 		try {
-			port = Integer.parseInt(args[0]);
-			server = new ServerSocket(port);
-		} catch (IllegalArgumentException e) {
-			throw new IllegalArgumentException("Format of argumant is not Integer!");
+			serverSocket = new ServerSocket(port);
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.out.println("Could not create server socket on port " + port + ". Quitting.");
+			System.exit(-1);
 		}
-		while (true) {
-			Socket newClient = null;
+
+		while (!terminate) {
 			try {
-				newClient = server.accept();
+				Socket clientSocket = serverSocket.accept();
+				new HandleConnection(clientSocket).start();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			new HandleConnection(newClient).start();
 		}
-		
+
+		try {
+			serverSocket.close();
+			System.out.println("Server Stopped");
+		} catch (Exception e) {
+			System.out.println("Error Found stopping server socket");
+			System.exit(-1);
+		}
 	}
-	
+
+	public static void main(String[] args) {
+		if (args.length > 1) {
+			throw new IllegalArgumentException("Too many argumants!");
+		}
+
+		try {
+			new Server(Integer.parseInt(args[0]));
+		} catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException("Format of argumant is not Integer!");
+		}
+	}
+
 	private class HandleConnection extends Thread {
 		Socket clientSocket = null;
 		BufferedReader input = null;
 		PrintStream output = null;
 		boolean terminate = false;
-		
+
 		public HandleConnection(Socket socket) {
 			this.clientSocket = socket;
 			try {
@@ -49,7 +66,7 @@ public class Server {
 				e.printStackTrace();
 			}
 		}
-		
+
 		public void terminate() {
 			terminate = true;
 		}
@@ -57,7 +74,27 @@ public class Server {
 		public void run() {
 			while (!terminate) {
 				if (clientSocket != null && output != null && input != null) {
-
+					String firstLine = input.readLine();
+					String[] splitLine = firstLine.split(" ");
+					String part1 = splitLine[0];
+					String part2 = null;
+					if (splitLine.length == 2) {
+						part2 = splitLine[1];
+					}
+					
+					switch(part1) {
+					case "W":
+						break;
+					case "P":
+						break;
+					case "T":
+						break;
+					case "L":
+						break;
+					default:
+						
+					}
+					
 				}
 			}
 		}
